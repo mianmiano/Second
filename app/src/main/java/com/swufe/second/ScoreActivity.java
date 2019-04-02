056a04928b0f4d4ba639831ed3f70891049551ee
mianmiano <fxr12047>
2019-04-02 20:42:54 +0800

    两队计分&利率换算&跳转页面

--- app/src/main/java/com/swufe/second/ScoreActivity.java
+++ app/src/main/java/com/swufe/second/ScoreActivity.java
@@ -12,6 +12,7 @@ import org.w3c.dom.Text;
 public class ScoreActivity extends AppCompatActivity {
 
     TextView tvScoreA;
+    TextView tvScoreB;
     String TAG = "ScoreActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,29 +20,53 @@ public class ScoreActivity extends AppCompatActivity {
         setContentView(R.layout.activity_score);
 
         tvScoreA = findViewById(R.id.tvScoreA);
+        tvScoreB = findViewById(R.id.tvScoreB);
 
     }
 
-    public void onClick(View btn){
+    public void btn1(View btn){
         if(btn.getId() == R.id.btnTeamA1){
             show(1);
-        }
-        if(btn.getId() == R.id.btnTeamA2){
-            show(2);
-        }
-        if(btn.getId() == R.id.btnTeamA3){
-            show(3);
-        }
-        if(btn.getId() == R.id.btnReset){
-            tvScoreA.setText("0");
+        }else{
+            show2(1);
         }
     }
+
+    public void btn2(View btn){
+        if(btn.getId() == R.id.btnTeamA2){
+            show(2);
+        }else{
+            show2(2);
+        }
+    }
+
+    public void btn3(View btn){
+        if(btn.getId() == R.id.btnTeamA3){
+            show(3);
+        }else{
+            show2(3);
+        }
+    }
+
+    public void reset(View btn){
+        tvScoreA.setText("0");
+        tvScoreB.setText("0");
+    }
+
 
     public void show(int i){
         String oldScore = (String) tvScoreA.getText();
         String newScore = String.valueOf(Integer.parseInt(oldScore)+i);
         Log.i(TAG, "show: score="+newScore);
         tvScoreA.setText(newScore);
+        Log.i(TAG, "show: score2="+newScore);
+    }
+
+    public void show2(int i){
+        String oldScore = (String) tvScoreB.getText();
+        String newScore = String.valueOf(Integer.parseInt(oldScore)+i);
+        Log.i(TAG, "show: score="+newScore);
+        tvScoreB.setText(newScore);
         Log.i(TAG, "show: score2="+newScore);
     }
 
