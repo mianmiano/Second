12c3a472e61e2bdfa1943b7ffda970d58a1de63f
mianmiano <fxr12047>
2019-05-21 21:10:15 +0800

    SQLite

--- app/src/main/java/com/swufe/second/MyListActivity.java
+++ app/src/main/java/com/swufe/second/MyListActivity.java
@@ -2,8 +2,10 @@ package com.swufe.second;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -26,7 +28,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -38,14 +42,20 @@ public class MyListActivity extends ListActivity implements Runnable,AdapterView
     private SimpleAdapter listItemAdapter;
 
     private String TAG = "MyListActivity";
+    private String logDate = "";
+    private final String DATE_SP_KEY = "lastRateDateStr";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_my_list);
 
-        listView = findViewById(R.id.myList);
-        String data[] = {"one", "two"};
+        SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
+        logDate = sp.getString(DATE_SP_KEY,"");
+        Log.i(TAG, "onCreate: logDate="+logDate);
+
+//        listView = findViewById(R.id.myList);
+//        String data[] = {"one", "two"};
 
         this.setListAdapter(listItemAdapter);
 
@@ -87,6 +97,10 @@ public class MyListActivity extends ListActivity implements Runnable,AdapterView
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 Log.i(TAG, "onClick: 对话框事件处理");
+                                //数据库删除对应数据
+                                RateManager manager = new RateManager(MyListActivity.this);
+                                manager.delete(position+1);
+                                Log.i("db"," 删除记录");
                                 listItems.remove(position);
                                 listItemAdapter.notifyDataSetChanged();
                             }
@@ -103,32 +117,65 @@ public class MyListActivity extends ListActivity implements Runnable,AdapterView
     public void run(){
         //获取网络数据，放入list带回主线程
         List<HashMap<String,String>> retList = new ArrayList<HashMap<String,String>>();
+        String curDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+        Log.i("run", "curDateStr:"+curDateStr+" logDate:"+logDate);
 
-        Document doc = null;
-        try {
-            //jsoup直接从网络地址获取
-            doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj").get();//获得connect对象，get方法获得对应doc
-            Log.i(TAG, "run: "+doc.title());
-            Elements tables = doc.getElementsByTag("table");
-            Element table = tables.get(1);
-            //获取td中的数据
-            Elements tds = table.getElementsByTag("td");
-
-            //创建list存放数据
-            for(int i=0;i<tds.size();i+=8){
-                Element td1 = tds.get(i);
-                Element td2 = tds.get(i+5);
-                String str = td1.text();
-                String val = td2.text();
-                Log.i(TAG, "run: "+str+"==>"+val);
+        //判断日期，从数据库/网络获取数据
+        if(curDateStr.equals(logDate)){
+            //日期相等，从数据库获取数据
+            Log.i("run", "日期相等，数据库获取数据");
+            RateManager manager = new RateManager(this);
+            for(RateItem item:manager.listAll()){
                 HashMap<String,String> map = new HashMap<>();
-                map.put("name",str);
-                map.put("rate",val);
+                map.put("name",item.getCurName());
+                map.put("rate",item.getCurRate());
                 retList.add(map);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+        }else{
+            //日期不等，从网络获取数据
+            Log.i("run", "日期不等，网络获取数据");
+            Document doc = null;
+            List<RateItem> rateList = new ArrayList<>();
+            try {
+                //jsoup直接从网络地址获取
+                doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj").get();//获得connect对象，get方法获得对应doc
+                Log.i(TAG, "run: "+doc.title());
+                Elements tables = doc.getElementsByTag("table");
+                Element table = tables.get(1);
+                //获取td中的数据
+                Elements tds = table.getElementsByTag("td");
+
+                //创建list存放数据
+                for(int i=0;i<tds.size();i+=8){
+                    Element td1 = tds.get(i);
+                    Element td2 = tds.get(i+5);
+                    String str = td1.text();
+                    String val = td2.text();
+                    Log.i(TAG, "run: "+str+"==>"+val);
+                    HashMap<String,String> map = new HashMap<>();
+                    map.put("name",str);
+                    map.put("rate",val);
+                    retList.add(map);
+                    rateList.add(new RateItem(str,val));
+                }
+                RateManager manager = new RateManager(this);
+                manager.deleteAll();
+                Log.i("db"," 删除所有记录");
+                manager.addAll(rateList);
+                Log.i("db"," 添加新记录集");
+
+                //更新sp的日期
+                SharedPreferences sp = getSharedPreferences("myrate",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString(DATE_SP_KEY,curDateStr);
+                editor.commit();
+                Log.i("run", "更新日期结束："+curDateStr);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
 
 
         Message msg = handler.obtainMessage(7);
--- app/src/main/java/com/swufe/second/RateListActivity.java
+++ app/src/main/java/com/swufe/second/RateListActivity.java
@@ -1,7 +1,9 @@
 package com.swufe.second;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -16,11 +18,16 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class RateListActivity extends ListActivity implements Runnable {
     private String TAG = "RateListActivity";
+    private String logDate = "";
+    private final String DATE_SP_KEY = "lastRateDateStr";
 
     Handler handler;
 
@@ -30,6 +37,9 @@ public class RateListActivity extends ListActivity implements Runnable {
         //因为ListActivity已经包含了布局，所以不需要通过页面填充它
         //setContentView(R.layout.activity_rate_list);
 
+        SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
+        logDate = sp.getString(DATE_SP_KEY, "");
+        Log.i ("List","lastRateDateStr=" +logDate);
 
         List<String> list1 = new ArrayList<>();
         for(int i=0;i<100;i++){
@@ -59,30 +69,64 @@ public class RateListActivity extends ListActivity implements Runnable {
         //获取网络数据，放入list带回主线程
         List<String> retList = new ArrayList<>();
 
-        Document doc = null;
-        try {
-            Thread.sleep(3000);
-            //jsoup直接从网络地址获取
-            doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj").get();//获得connect对象，get方法获得对应doc
-            Log.i(TAG, "run: "+doc.title());
-            Elements tables = doc.getElementsByTag("table");
-            Element table = tables.get(1);
-            //获取td中的数据
-            Elements tds = table.getElementsByTag("td");
+        //判断日期
+        String curDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+        Log.i("run", "curDateStr:"+curDateStr+" logDate:"+logDate);
 
-            //创建list存放数据
-            for(int i=0;i<tds.size();i+=8){
-                Element td1 = tds.get(i);
-                Element td2 = tds.get(i+5);
-                String str = td1.text();
-                String val = td2.text();
-                Log.i(TAG, "run: "+str+"==>"+val);
-                retList.add(str+"==>"+val);
+        if(curDateStr.equals(logDate)){
+            //日期相等，数据库获取数据
+            Log.i("run", "日期相等，数据库获取数据");
+            RateManager manager = new RateManager(this);
+            for(RateItem item : manager.listAll()){
+                retList.add(item.getCurName()+"==>"+item.getCurRate());
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        }else{
+            //日期不等，网络获取数据
+            Log.i("run", "日期不等，网络获取数据");
+            Document doc = null;
+            //存入数据库
+            List<RateItem> rateList = new ArrayList<>();
+            try {
+                Thread.sleep(300);
+                //jsoup直接从网络地址获取
+                doc = Jsoup.connect("http://www.boc.cn/sourcedb/whpj").get();//获得connect对象，get方法获得对应doc
+                Log.i(TAG, "run: "+doc.title());
+                Elements tables = doc.getElementsByTag("table");
+                Element table = tables.get(1);
+                //获取td中的数据
+                Elements tds = table.getElementsByTag("td");
+
+                //创建list存放数据
+                for(int i=0;i<tds.size();i+=8){
+                    Element td1 = tds.get(i);
+                    Element td2 = tds.get(i+5);
+                    String str = td1.text();
+                    String val = td2.text();
+                    Log.i(TAG, "run: "+str+"==>"+val);
+                    retList.add(str+"==>"+val);
+
+                    RateItem item = new RateItem(str,val);
+                    rateList.add(item);
+                }
+
+                RateManager manager = new RateManager(this);
+                manager.deleteAll();
+                Log.i("db"," 删除所有记录");
+                manager.addAll(rateList);
+                Log.i("db"," 添加新记录集");
+
+                //修改日期，写入sp
+                SharedPreferences sp = getSharedPreferences("myrate",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString(DATE_SP_KEY,curDateStr);
+                editor.commit();
+                Log.i("run", "更新日期结束："+curDateStr);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
 
